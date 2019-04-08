634f20b5ac1b813cbc3189bebed7df1c5fa813bf
Manuel <gitlab@ntlx.org>
2019-04-08 22:59:03 +0200

    csv printer

---
+++ 03-GPIO/dht_csv.py
@@ -0,0 +1,23 @@
+import RPi.GPIO as GPIO
+import Adafruit_DHT
+import time
+import datetime
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.OUT)
+
+sensor = Adafruit_DHT.DHT11
+pin = 27
+
+try:
+    for i in range(21):
+        GPIO.output(17, GPIO.HIGH)
+        humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+        now = datetime.datetime.now()
+        now_iso = now.isoformat()
+        print("%s,%s,%s" % (now_iso, humidity, temperature))
+        GPIO.output(17, GPIO.LOW)
+        time.sleep(10)
+finally:
+    GPIO.cleanup()
